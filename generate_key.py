0a86d6ad874745e8eccce2a3e7dca7f22b2ccb08
Ackinator <sterneraxel@protonmail.com>
2024-10-10 14:39:43 +0200

    Gjort klart generate_key, börjat på crypto_tool

--- generate_key.py
+++ generate_key.py
@@ -1,2 +1,6 @@
-import cryptography
-from cryptography import fernet
+from cryptography.fernet import Fernet
+
+key = Fernet.generate_key() # genererar nyckeln
+
+with open("secret.key", "wb") as key_file:
+    key_file.write(key) # sparar nyckeln som en fil
