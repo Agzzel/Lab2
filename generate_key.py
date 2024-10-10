337e42e79a2e3fcbeae8780ff4d8d702e7018e3e
Ackinator <sterneraxel@protonmail.com>
2024-10-10 16:39:47 +0200

    Skapat funktioner

--- generate_key.py
+++ generate_key.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 
-key = Fernet.generate_key() # genererar nyckeln
-
-with open("secret.key", "wb") as key_file:
-    key_file.write(key) # sparar nyckeln som en fil
+def create_key():
+    key = Fernet.generate_key() # genererar nyckeln
+    with open("secret.key", "wb") as key_file:
+        key_file.write(key) # sparar nyckeln som en fil
