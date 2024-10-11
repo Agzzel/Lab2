d14787966e8c1babf9498134b44f1c7e7138fd33
Ackinator <sterneraxel@protonmail.com>
2024-10-11 09:27:41 +0200

    crypto_tool fungerar med hårdkodade värden

--- crypto_tool.py
+++ crypto_tool.py
@@ -1,20 +1,23 @@
 from generate_key import create_key
 from cryptography.fernet import Fernet
 
+create_key()
+with open("secret.key", "rb") as key_file:
+    key = key_file.read()
+
 def encrypt():
-    create_key()
-    with open("secret.key", "rb") as key_file:
-        key = key_file.read()
         cipher = Fernet(key) # instansierar Fernet med key som argument
         message = input("Ange meddelande: ").encode()
         cipher_text = cipher.encrypt(message)
-    with open("encoded_msg.enc", "wb") as encoded_file:
-        encoded_file.write(cipher_text) # sparar det krypterade meddelandet som en fil
+        with open("encoded_msg.enc", "wb") as encoded_file:
+            encoded_file.write(cipher_text) # sparar det krypterade meddelandet som en fil
 
 
 def decrypt():
     with open("encoded_msg.enc", "rb") as encrypted_file:
         message = encrypted_file.read() # spara det krypterade meddelandet i message
+        cipher = Fernet(key)
+        cipher_text = cipher.decrypt(message)
+        print(cipher_text)
 
-encrypt()
-decrypt()
+
