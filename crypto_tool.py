56f6720ebcaf32988dab740aa443c4b0abd145e6
Ackinator <sterneraxel@protonmail.com>
2024-10-14 13:10:52 +0200

    Gjort klart crypto_tool

--- crypto_tool.py
+++ crypto_tool.py
@@ -1,23 +1,54 @@
 from generate_key import create_key
 from cryptography.fernet import Fernet
+import argparse
 
-create_key()
-with open("secret.key", "rb") as key_file:
-    key = key_file.read()
+def generate_key(key_name):
+    create_key(key_name)
 
-def encrypt():
-        cipher = Fernet(key) # instansierar Fernet med key som argument
-        message = input("Ange meddelande: ").encode()
-        cipher_text = cipher.encrypt(message)
-        with open("encoded_msg.enc", "wb") as encoded_file:
-            encoded_file.write(cipher_text) # sparar det krypterade meddelandet som en fil
+def encrypt(message, keyInput):
+    with open(keyInput, "rb") as keyFile:
+        key = keyFile.read()
+    cipher = Fernet(key) 
+    message = message.encode()
+    cipher_text = cipher.encrypt(message)
+    fileName = input("Enter the name to save the encrypted file as (must end in .enc):")
+    with open(fileName, "wb") as encoded_file:
+        encoded_file.write(cipher_text)
 
 
-def decrypt():
-    with open("encoded_msg.enc", "rb") as encrypted_file:
-        message = encrypted_file.read() # spara det krypterade meddelandet i message
-        cipher = Fernet(key)
-        cipher_text = cipher.decrypt(message)
-        print(cipher_text)
+def decrypt(fileName, key):
+    with open(fileName, "rb") as encrypted_message:
+        message = encrypted_message.read()
+    with open(key, "rb") as key_file:
+        key = key_file.read()
+    cipher_suite = Fernet(key)
+    plain_text = cipher_suite.decrypt(message)
+    plainTextFile = open("output.txt","w")
+    plainTextFile.write(plain_text.decode())
+    plainTextFile.close()
+
+parser = argparse.ArgumentParser()
+subparsers = parser.add_subparsers(dest="command")
+
+key_parser = subparsers.add_parser("generate", help="Generate an encryption key")
+key_parser.add_argument("name", type=str, help="name of the key file")
+
+encrypt_parser = subparsers.add_parser("encrypt", help="Encrypt a text string")
+encrypt_parser.add_argument("message", type=str, help="text to encrypt")
+encrypt_parser.add_argument("key", type=str, help="encryption key")
+
+decrypt_parser = subparsers.add_parser("decrypt", help="Decrypt an encrypted file")
+decrypt_parser.add_argument("filename", type=str, help="File to decrypt")
+decrypt_parser.add_argument("key", type=str, help="Decryption key")
+
+args = parser.parse_args()
+if args.command == "encrypt":
+    encrypt(args.message, args.key)
+elif args.command == "decrypt":
+    decrypt(args.filename, args.key)
+elif args.command == "generate":
+    generate_key(args.name)
+
+
 
 
--- generate_key.py
+++ generate_key.py
@@ -1,6 +1,6 @@
 from cryptography.fernet import Fernet
 
-def create_key():
+def create_key(key_name):
     key = Fernet.generate_key() # genererar nyckeln
-    with open("secret.key", "wb") as key_file:
+    with open(key_name, "wb") as key_file:
         key_file.write(key) # sparar nyckeln som en fil
